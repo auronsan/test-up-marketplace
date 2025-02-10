edd806306f699fe7658df9af35d3c027ec539a34
auronsan <auronsanjr@gmail.com>
2025-02-11 01:03:57 +0700

    ♻️ clean console

--- app/utils/use-axios.ts
+++ app/utils/use-axios.ts
@@ -3,7 +3,6 @@ import axios from "axios";
 export const axiosClient = axios.create({
   baseURL: process.env.NEXT_PUBLIC_BASE_URL,
 });
-console.log(process.env.NEXT_PUBLIC_BASE_URL, "bcd");
 export const useAxios = () => {
   return axiosClient;
 };
