9a98eef08118c8689185880cf2eef1f92c757fb7
auronsan <auronsanjr@gmail.com>
2025-02-11 02:06:59 +0700

    🚨 fix config eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -15,7 +15,8 @@ const eslintConfig = [
     rules: {
       "@typescript-eslint/no-explicit-any": "off",
       "react-hooks/exhaustive-deps": "off",
-      "@typescript-eslint.io/rules/no-unused-vars": "warn",
+      "no-unused-vars": "off",
+      "@typescript-eslint/no-unused-vars": "warn",
     },
   },
 ];
